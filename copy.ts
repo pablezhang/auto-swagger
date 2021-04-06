3fb1728dd8d5c7e61cb4f9c3b24dfeace3feca93
luckybo <luckybo0027@gmail.com>
2021-04-06 22:42:30 +0800

    修改copy.ts为dist文件夹复制package.json与readme.md

--- copy.ts
+++ copy.ts
@@ -1,5 +1,23 @@
-import fs from 'fs';
-import * as path from "path";
+import * as fs from 'fs';
+
+
 fs.copyFile('src/configs/swagger.config.js', 'dist/configs/swagger.config.js', function (err) {
-  console.log(err, 'err')
+  err && console.log(err, 'err')
 });
+
+
+fs.copyFile('README.md', 'dist/README.md', function (err) {
+  err && console.log(err, 'err')
+});
+
+
+const pkg = require('./package.json');
+
+delete pkg.devDependencies
+pkg.main = 'index.js';
+pkg.bin['auto-swagger'] = 'index.js';
+
+fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, 2))
+console.log('复制成功')
+
+
